7bce3173d9ea42227190163c4b6ef66de8264bd4
AustinPotts <pottsevolvements@gmail.com>
2020-07-08 11:08:31 -0400

    Configured Inner Gradients

--- NeuMorph/NeuMorph/ContentView.swift
+++ NeuMorph/NeuMorph/ContentView.swift
@@ -12,6 +12,41 @@ extension Color {
     static let offWhite = Color(red: 225 / 225, green: 225 / 255, blue: 235 / 255)
 }
 
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .contentShape(Circle())
+        .background(
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                        .fill(Color.offWhite)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 4)
+                            .blur(radius: 4)
+                            .offset(x: 2, y: 2)
+                            .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                    )
+                } else {
+            Circle()
+                .fill(Color.offWhite)
+                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+                
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         ZStack {
@@ -22,6 +57,14 @@ struct ContentView: View {
                 .frame(width: 300, height: 300)
                 .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
                 .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            
+            Button(action: {
+                print("Boom")
+            }) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.gray)
+            }
+        .buttonStyle(SimpleButtonStyle())
         }
         .edgesIgnoringSafeArea(.all)
     }
